1c7e41e66f0831e731b30d7689011f018b348f2e
Kyle Bedell <kyle.bedell@gmail.com>
2015-12-22 16:44:02 +0900

    Clean up CSS selectors

--- get.rb
+++ get.rb
@@ -9,17 +9,17 @@ site_url = 'http://www.tripadvisor.com/RestaurantsNear-g294305-d2538166-Plaza_Nu
 
 doc = Nokogiri::HTML(open(site_url))
 
-locality = ' ' + doc.css("[class=locality]").text
-locality += doc.css("[class=country-name]").text
-title = doc.css("[id=HEADING]").text
-listings = doc.css("div[class=near_listing]")
+locality = ' ' + doc.css('.locality').text
+locality += doc.css('.country-name').text
+title = doc.css('#HEADING').text
+listings = doc.css('div.near_listing')
 
 locations = []
 bar = ProgressBar.create(:title => "Locating", :total => 15)
 
 listings[0..14].each { |l|
-    place = l.css("[class=location_name]").text
-    address = l.css("[class=format_address]").text + locality
+    place = l.css('.location_name').text
+    address = l.css('.format_address').text + locality
     x = Hash.new
     x = Geocoder.search(place + ' ' + address)[0].data['geometry']['location']
     x['name'] = place
