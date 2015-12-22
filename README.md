85c5b15e71519c881de7c5c0b38e0ae91bb5f1ea
Kyle Bedell <kyle.bedell@gmail.com>
2015-12-22 13:58:57 +0900

    First commit

---
+++ README.md
@@ -0,0 +1,15 @@
+TripAdvisor Top 10 List to KML
+==========================
+
+Ruby script to scrape a TripAdvisor Top 10 list, take the top 15 listed locations, and add them to a KML file for importing into Google Maps.
+
+Usage
+----------
+
+Put your site URL in line 7 and run the script.
+
+To do
+----------
+
+Add comments to each entry
+Add an icon for Google maps
---
+++ get.rb
@@ -0,0 +1,41 @@
+require 'open-uri'
+require 'geocoder'
+require 'ruby_kml'
+require 'nokogiri'
+require 'ruby-progressbar'
+
+site_url = 'http://www.tripadvisor.com/RestaurantsNear-g294305-d2538166-Plaza_Nunoa-Santiago_Santiago_Metropolitan_Region.html'
+
+doc = Nokogiri::HTML(open(site_url))
+
+locality = ' ' + doc.css("[class=locality]").text
+locality += doc.css("[class=country-name]").text
+title = doc.css("[id=HEADING]").text
+listings = doc.css("div[class=near_listing]")
+
+locations = []
+bar = ProgressBar.create(:title => "Locating", :total => listings.size)
+
+listings[0..14].each { |l|
+    place = l.css("[class=location_name]").text
+    address = l.css("[class=format_address]").text + locality
+    x = Hash.new
+    x = Geocoder.search(place + ' ' + address)[0].data['geometry']['location']
+    x['name'] = place
+    locations << x
+    bar.increment
+    sleep(0.25)
+}
+
+kml = KMLFile.new
+folder = KML::Folder.new(:name => 'Places')
+locations.each { |l|
+    folder.features << KML::Placemark.new(
+        :name => l['name'],
+        #'styleUrl' => '#icon-1085-nodesc',
+        #:description => 'Attached to the ground. Intelligently places itself at the height of the underlying terrain.',
+        :geometry => KML::Point.new(:coordinates=>l['lng'].to_s + ',' + l['lat'].to_s + ',0.0')
+    )
+}
+kml.objects << folder
+kml.save 'stuff.kml'
